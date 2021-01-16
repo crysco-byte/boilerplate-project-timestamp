7435d13220a37a131fd84587414ad4f902add436
cysco-byte <cryptoescor@protonmail.com>
2021-01-16 15:00:19 -0300

    test

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("hello");
-});
-
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
